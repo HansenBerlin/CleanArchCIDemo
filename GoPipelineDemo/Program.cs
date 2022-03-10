e80da57780d0abcf2b409d1864b0af9e815e1c6c
LT-6FGD373\Hannes <h.roever@zertificon.com>
2022-03-10 17:40:07 +0100

    test

--- GoPipelineDemo/Program.cs
+++ GoPipelineDemo/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace GoPipelineDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
+
