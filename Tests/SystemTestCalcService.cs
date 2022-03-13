d30d48cf9d2a060cea47ea73a30ea6b38c5334a8
LT-6FGD373\Hannes <h.roever@zertificon.com>
2022-03-13 12:26:13 +0100

    fix #1

--- Tests/SystemTestCalcService.cs
+++ Tests/SystemTestCalcService.cs
@@ -25,4 +25,16 @@ public class SystemTestCalcService
         
         Assert.Equal(PrimeState.IsPrime, result.Result);
     }
+    
+    [Theory]
+    [InlineData(2)]
+    [InlineData(100)]
+    [InlineData(300000000)]
+    public void CheckPrime_ShouldReturnOnlyIsNoPrime_WhenValidNonPrimeNumberIsPassed(long checkNumber)
+    {
+        var sut = new Calculator();
+        var result = sut.CheckPrime(checkNumber);
+        
+        Assert.Equal(PrimeState.IsNoPrime, result.Result);
+    }
 }
