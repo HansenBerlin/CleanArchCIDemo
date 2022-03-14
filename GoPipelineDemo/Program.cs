f8d2be69c2f73d235c6efb1839015d86487ba818
LT-6FGD373\Hannes <h.roever@zertificon.com>
2022-03-14 11:05:22 +0100

    add new projects

--- GoPipelineDemo/Program.cs
+++ GoPipelineDemo/Program.cs
@@ -6,15 +6,7 @@ namespace GoPipelineDemo
     {
         static void Main(string[] args)
         {
-            var calc = new Calculator();
-            Console.WriteLine(calc.Add(2, 2));
-            Console.WriteLine(calc.CheckPrime(3).Result);
-            Console.WriteLine(calc.CheckPrime(13).Result);
-            Console.WriteLine(calc.CheckPrime(305175781).Result);
-            Console.WriteLine(calc.CheckPrime(1).Result);
-            Console.WriteLine(calc.CheckPrime(12).Result);
-            Console.WriteLine(calc.CheckPrime(10000000).Result);
-            
+
         }
     }
 }
